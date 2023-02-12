568b92fb2d32ffafc8647244b2a0c7434f2b5c3d
unknown <huynhvangiang0504@example.com>
2023-02-12 17:03:25 +0700

    third

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 document.getElementById("btn").onclick = function(){
-    var year = document.getElementById("namDuong").value;
-    year = Number(year);
-    if(year > 0){
-        
-        var cans = ["Quý","Giáp","Ất","Bính","Đinh","Mậu",
-        "Kỳ","Canh","Tân","Nhâm"];
-        var chis = ["Hợi","Tý","Sửu","Dần","Mão","Thìn","Tỵ"
-                    ,"Ngọ","Mùi","Thân","Dậu","Tuất"
-                   ];
-        var res = cans[(year-3)%10] + " " + chis[(year-3)%12];
-        document.getElementById("kq").innerHTML = res;
+    var thang = document.getElementById("month").value;
+    var nam = document.getElementById("year").value;
+    thang = Number(thang);
+    nam = Number(nam);
+    var res;
+    if((thang < 1 || thang > 12)){
+        alert("Nhập lại mày");
+        document.getElementById("kq").innerHTML = "";
+    }
+    else if(nam <= 0){
+        alert("Nhập lại mày");
+        document.getElementById("kq").innerHTML = "";
     }
     else{
-        alert("Nhập lại");
-    } 
-   
+        if(thang == 2){
+            if(nam%400 == 0 || nam%4==0 && nam%100!=0) res = 29;
+            else res = 28;
+        }
+        else if(thang == 4 || thang == 6 || thang == 9 || thang == 11){
+            res = 30;
+        }
+        else{
+            res = 31;
+        }
+        document.getElementById("kq").innerHTML = "Tháng " + thang
+                                                 + " năm " + nam +
+                                                 " có " + res +
+                                                  " ngày"; 
+    }
 }
